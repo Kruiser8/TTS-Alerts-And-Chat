7295b7d104b45373f8963e48b4ba9ca50eded5bd
Kruiser8 <17681476+Kruiser8@users.noreply.github.com>
2020-01-26 16:13:39 -0500

    Initial commit

---
+++ README.txt
@@ -0,0 +1,129 @@
+# TTS Alerts And Chat
+
+TTS Alerts And Chat is a Streamlabs Chatbot script that provides text-to-speech capabilities for Streamlabs alerts, chat messages, and a customizable command.
+
+The text-to-speech uses your computer's narrator voice and has some customization for voice, volume, and speech rate.
+
+Feel free to reach out to me in the Streamlabs Chatbot discord (@Kruiser8) or on Twitter (@Kruiser8) with any questions or feedback.
+
+# Credits
+
+Thanks to [Ocgineer](https://github.com/ocgineer) for his [Streamlabs Event Receiver](https://github.com/ocgineer/Streamlabs-Events-Receiver) that allows events to be caught in a Streamlabs Chatbot script.
+
+# Installation
+
+For chatbot script installation help, please view this guide:
+https://github.com/StreamlabsSupport/Streamlabs-Chatbot/wiki/Prepare-&-Import-Scripts
+
+# Usage
+
+Before the alert text-to-speech will work, you'll need to add a Streamlabs Socket API token.
+
+- In Streamlabs Chatbot, open the *Scripts* tab
+- Click the *TTS Alerts And Chat* script in order to open the settings sidebar
+- At the bottom, select the **Streamlabs Token** dropdown
+- Click the *Get Socket Token* button or go to https://streamlabs.com/dashboard#/settings/api-settings in a browser.
+- In the browser, click the **API Tokens** tab
+- Copy the **Your Socket API Token**
+- Paste this value into the **Streamlabs Socket Token** setting in the chatbot
+- Click the *Save Settings* button at the bottom of the script settings
+
+# Setting Information
+
+All settings have extra information if you hover over the field in the chatbot. Below is a summarized list of settings.
+
+## Text-To-Speech Settings
+All TTS Voice settings are in this section.
+
+### Voice Name
+Set the narrator voice to use. This is a list of the narrator voices on your computer.
+
+### Volume
+Set the volume of the narrator.
+
+### Speaking Rate
+Set the pace of how fast the narrator should speak.
+
+## TTS Command
+In this section, you can customize a command that uses text-to-speech.
+
+### Command Name
+Set the command to use for text-to-speech.
+
+### Permission level
+Set the viewer rank/role required to use the TTS command.
+
+### Permission level info
+Set the user, rank, or tier for permission. Only used with certain permission levels: User_Specific, Min_Rank, Min_Points, Min_Hours.
+
+### Cost (Points)
+The number of points required to use the TTS command.
+
+### TTS Message Format
+Customize the message read by the narrator. The following variables will get replaced.
+- *{user}* will be replaced with the viewer's username
+- *{message}* will be replaced with the chat message
+
+## TTS Cooldown
+In this section, you can customize a cooldown for the TTS Command that uses text-to-speech.
+
+### Caster ignores cooldown
+Enables the Caster to ignore the cooldown.
+
+### Cooldown in seconds
+Set the number of seconds before the TTS command can be used again.
+
+### Cooldown Response
+The message that the bot will display when the command is on cooldown.
+- {user} will be replaced with the viewer's username
+- {cooldown} will be replaced with the time remaining
+
+### User cooldown in seconds
+Set the number of seconds before a user can use the TTS command again.
+
+### User Cooldown Response
+The message that the bot will display when the command is on user cooldown
+- {user} will be replaced with the viewer's username
+- {cooldown} will be replaced with the time remaining
+
+## TTS Chat Messages
+In this section, you can toggle text-to-speech for ALL chat messages
+
+### TTS All Chat Messages
+Enable Text-To-Speech for all messages
+
+### TTS Message Format
+Customize the message read by the narrator. The following variables will get replaced.
+- *{user}* will be replaced with the viewer's username
+- *{message}* will be replaced with the chat message
+
+## Alert Types
+There are alert types across Mixer, Streamlabs, Twitch, and Youtube that are customizable. To save space, below is a summary of setting types.
+
+### Enabled
+Enable TTS for the given alert.
+
+### Delay (seconds)
+Delay in seconds to read the alert after receiving it. If two alerts are received simultaneously, the second's delay may be lost.
+
+This setting is an attempt to delay TTS until after alert sfx is finished. Unfortunately, the script receives alerts *instantly* as opposed to through the queue that Streamlabs provides.
+
+### Alert Message
+The format of the alert to be read by text-to-speech. There are a number of variables available per alert type. Hover over the message in the settings for a detailed list for that alert.
+- *{name}* will be replaced with the username
+- *{amount}* will be replaced with a number for that alert (viewers, raiders, bits, etc.)
+- *{isPlural}* is sometimes used to provide an 's' when amount > 1
+- *{tier}* is the Twitch subscription tier
+- *{months}* is the number of months of a subscription
+- *{gifter}* is the gifter of a Twitch sub
+
+## Streamlabs Token
+
+### Streamlabs Socket Token
+**Your Socket API Token** from https://streamlabs.com/dashboard#/settings/api-settings. See the *Usage* section for details.
+
+# Copyright
+
+This alert was made by **Kruiser8** (https://twitch.tv/kruiser8) and is licensed under the *Creative Commmons Attribution 4.0 International License (CC BY 4.0)*
+
+For License information, visit https://creativecommons.org/licenses/by/4.0/
---
+++ TTSAlertsAndChat_StreamlabsSystem.py
@@ -0,0 +1,503 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+""" Text-To-Speech for Alerts and Chat Messages
+
+	1.0.0
+		Initial public release
+
+"""
+
+#---------------------------------------
+# Script Import Libraries
+#---------------------------------------
+import os
+import codecs
+import json
+from collections import OrderedDict
+import time
+
+import clr
+clr.AddReference("IronPython.Modules.dll")
+clr.AddReference('System.Speech')
+clr.AddReferenceToFileAndPath(os.path.join(os.path.dirname(os.path.realpath(__file__)), "StreamlabsEventReceiver.dll"))
+from System.Speech.Synthesis import SpeechSynthesizer
+from StreamlabsEventReceiver import StreamlabsEventClient
+
+#---------------------------------------
+# Script Information
+#---------------------------------------
+ScriptName = "TTS Alerts and Chat"
+Website = "https://www.twitch.tv/kruiser8"
+Description = "Text-to-speech for streamlabs alerts and chat messages."
+Creator = "Kruiser8"
+Version = "1.0.0"
+
+#---------------------------------------
+# Script Variables
+#---------------------------------------
+
+# Socket Receiver
+EventReceiver = None
+
+# Settings file location
+SettingsFile = os.path.join(os.path.dirname(__file__), "settings.json")
+
+# UI Config file location
+UIConfigFile = os.path.join(os.path.dirname(__file__), "UI_Config.json")
+
+SubPlanMap = {
+	"Prime": "Prime",
+	"1000": "Tier 1",
+	"2000": "Tier 2",
+	"3000": "Tier 3"
+}
+
+#---------------------------------------
+# Script Classes
+#---------------------------------------
+class Settings(object):
+	""" Load in saved settings file if available else set default values. """
+	def __init__(self, settingsfile=None):
+		try:
+			with codecs.open(settingsfile, encoding="utf-8-sig", mode="r") as f:
+				self.__dict__ = json.load(f, encoding="utf-8")
+		except:
+			self.VoiceName = ''
+			self.Volume = 80
+			self.Rate = 0
+			self.TTSCommand = "!tts"
+			self.TTSCommandPermission = "Caster"
+			self.TTSCommandPermissionInfo = ""
+			self.TTSCommandCost = 500
+			self.TTSUseCD = False
+			self.TTSCasterCD = True
+			self.TTSCooldown = 0
+			self.TTSOnCooldown = "{user} the command is still on cooldown for {cooldown} seconds!"
+			self.TTSUserCooldown = 10
+			self.TTSOnUserCooldown = "{user} the command is still on user cooldown for {cooldown} seconds!"
+			self.TTSAllChat = False
+			self.TTSAllChatMessage = "{user} says, {message}"
+			self.MixerOnFollow = True
+			self.MixerFollowDelay = 0
+			self.MixerFollowMessage = "{name} has followed."
+			self.MixerOnHost = True
+			self.MixerHostMinimum = 0
+			self.MixerHostDelay = 0
+			self.MixerHostMessage = "{name} has hosted you with {amount} viewer{isPlural}."
+			self.MixerOnSub = True
+			self.MixerIncludeSubMessage = True
+			self.MixerSubDelay = 0
+			self.MixerSubMessage = "{name} has subscribed ({tier})."
+			self.MixerResubMessage = "{name} has resubscribed ({tier}) for {months} months."
+			self.StreamlabsOnDonation = True
+			self.StreamlabsIncludeDonationMessage = True
+			self.StreamlabsDonationMinimum = 5
+			self.StreamlabsDonationDelay = 0
+			self.StreamlabsDonationMessage = "{name} donated {amount}."
+			self.TwitchOnCheer = True
+			self.TwitchIncludeCheerMessage = True
+			self.TwitchCheerMinimum = 100
+			self.TwitchCheerDelay = 0
+			self.TwitchCheerMessage = "{name} has used {amount} bit{isPlural}."
+			self.TwitchOnFollow = True
+			self.TwitchFollowDelay = 0
+			self.TwitchFollowMessage = "{name} has followed."
+			self.TwitchOnHost = True
+			self.TwitchHostMinimum = 0
+			self.TwitchHostDelay = 0
+			self.TwitchHostMessage = "{name} has hosted you with {amount} viewer{isPlural}."
+			self.TwitchOnRaid = True
+			self.TwitchRaidMinimum = 0
+			self.TwitchRaidDelay = 0
+			self.TwitchRaidMessage = "{name} has raided you with a party of {amount}."
+			self.TwitchOnSub = True
+			self.TwitchIncludeSubMessage = True
+			self.TwitchSubDelay = 0
+			self.TwitchSubMessage = "{name} has subscribed ({tier})."
+			self.TwitchResubMessage = "{name} has resubscribed ({tier}) for {months} months."
+			self.TwitchGiftMessage = "{gifter} has gifted a sub ({tier}) to {name} ({months} month{isPlural})."
+			self.YoutubeOnFollow = True
+			self.YoutubeFollowDelay = 0
+			self.YoutubeFollowMessage = "{name} has followed."
+			self.YoutubeOnSub = True
+			self.YoutubeIncludeSubMessage = True
+			self.YoutubeSubDelay = 0
+			self.YoutubeSubMessage = "{name} has subscribed ({tier})."
+			self.YoutubeResubMessage = "{name} has resubscribed ({tier}) for {months} months."
+			self.YoutubeOnSuperchat = True
+			self.YoutubeIncludeSuperchatMessage = True
+			self.YoutubeSuperchatMinimum = 5
+			self.YoutubeSuperchatDelay = 0
+			self.YoutubeSuperchatMessage = "{name} donated {amount}."
+			self.SocketToken = None
+
+	def Reload(self, jsondata):
+		""" Reload settings from Streamlabs user interface by given json data. """
+		self.__dict__ = json.loads(jsondata, encoding="utf-8")
+
+	def Save(self, settingsfile):
+		""" Save settings contained within to .json and .js settings files. """
+		try:
+			with codecs.open(settingsfile, encoding="utf-8-sig", mode="w+") as f:
+				json.dump(self.__dict__, f, encoding="utf-8", ensure_ascii=False)
+			with codecs.open(settingsfile.replace("json", "js"), encoding="utf-8-sig", mode="w+") as f:
+				f.write("var settings = {0};".format(json.dumps(self.__dict__, encoding='utf-8', ensure_ascii=False)))
+		except:
+			Parent.Log(ScriptName, "Failed to save settings to file.")
+
+class UIConfig(object):
+	""" Load in saved settings file if available else set default values. """
+	def __init__(self, uiconfigfile=None):
+		try:
+			with codecs.open(uiconfigfile, encoding="utf-8-sig", mode="r") as f:
+				self.__dict__ = json.load(f, encoding="utf-8", object_pairs_hook=OrderedDict)
+		except:
+			Parent.SendStreamWhisper(Parent.GetChannelName(), "Failed to read UIConfig file: " + str(sys.exc_info()[1]))
+
+	def Save(self, uiconfigfile):
+		""" Save UI Config contained within to .json file. """
+		if len(self.__dict__) > 0:
+			try:
+				with codecs.open(uiconfigfile, encoding="utf-8-sig", mode="w+") as f:
+					json.dump(self.__dict__, f, encoding="utf-8", ensure_ascii=False)
+			except:
+				Parent.SendStreamWhisper(Parent.GetChannelName(), "Failed to save ui config to file.")
+
+#---------------------------------------
+# Event Receiver Functions
+#---------------------------------------
+
+def EventReceiverConnected(sender, args):
+	Parent.Log(ScriptName, "Connected")
+	return
+
+def EventReceiverDisconnected(sender, args):
+	Parent.Log(ScriptName, "Disconnected")
+
+def EventReceiverEvent(sender, args):
+	handleEvent(sender,args)
+
+def handleEvent(sender, args):
+	# Just grab the all data in from the event
+	evntdata = args.Data
+
+	# Check if it contains data and for what streaming service it is
+	if evntdata and evntdata.For == "twitch_account":
+
+		# This is an Twitch follow event
+		if evntdata.Type == "follow" and ScriptSettings.TwitchOnFollow:
+			# Events can come in bulk so it is in a list, itterate over it.
+			for message in evntdata.Message:
+				ttsMessage = ScriptSettings.TwitchFollowMessage.format(name=message.Name)
+				SendTTSMessagesWithDelay(ttsMessage, ScriptSettings.TwitchFollowDelay)
+
+		# This is a Twitch cheer event
+		elif evntdata.Type == "bits" and ScriptSettings.TwitchOnCheer:
+			s = ''
+			for message in evntdata.Message:
+				if message.Amount >= ScriptSettings.TwitchCheerMinimum:
+					if message.Amount > 1:
+						s = 's'
+					else:
+						s = ''
+					ttsMessage = ScriptSettings.TwitchCheerMessage.format(name=message.Name, amount=message.Amount, isPlural=s)
+					SendTTSMessagesWithDelay(ttsMessage, ScriptSettings.TwitchCheerDelay, ScriptSettings.TwitchIncludeCheerMessage, message.Message)
+
+		elif evntdata.Type == "host" and ScriptSettings.TwitchOnHost:
+			s = ''
+			for message in evntdata.Message:
+				if int(message.Viewers) >= ScriptSettings.TwitchHostMinimum:
+					if message.Viewers > 1:
+						s = 's'
+					else:
+						s = ''
+					ttsMessage = ScriptSettings.TwitchHostMessage.format(name=message.Name, amount=str(message.Viewers), isPlural=s)
+					SendTTSMessagesWithDelay(ttsMessage, ScriptSettings.TwitchHostDelay)
+
+		elif evntdata.Type == "raid" and ScriptSettings.TwitchOnRaid:
+			for message in evntdata.Message:
+				if int(message.Raiders) >= ScriptSettings.TwitchRaidMinimum:
+					ttsMessage = ScriptSettings.TwitchRaidMessage.format(name=message.Name, amount=str(message.Raiders))
+					SendTTSMessagesWithDelay(ttsMessage, ScriptSettings.TwitchRaidDelay)
+
+		# This is a Twitch subscription event
+		elif evntdata.Type == "subscription" and ScriptSettings.TwitchOnSub:
+			s = ''
+			for message in evntdata.Message:
+				tier = SubPlanMap[str(message.SubPlan)]
+				ttsMessage = ''
+				if message.Gifter:
+					if message.Months > 1:
+						s = 's'
+					else:
+						s = ''
+					ttsMessage = ScriptSettings.TwitchGiftMessage.format(name=message.Name, gifter=message.Gifter, tier=tier, months=message.Months, isPlural=s)
+				else:
+					if message.Months == 1:
+						ttsMessage = ScriptSettings.TwitchSubMessage.format(name=message.Name, tier=tier, months=message.Months)
+					else:
+						ttsMessage = ScriptSettings.TwitchResubMessage.format(name=message.Name, tier=tier, months=message.Months)
+
+				SendTTSMessagesWithDelay(ttsMessage, ScriptSettings.TwitchSubDelay, ScriptSettings.TwitchIncludeSubMessage, message.Message)
+
+	elif evntdata and evntdata.For == "mixer_account":
+
+		# This is an Twitch follow event
+		if evntdata.Type == "follow" and ScriptSettings.MixerOnFollow:
+			# Events can come in bulk so it is in a list, itterate over it.
+			for message in evntdata.Message:
+				ttsMessage = ScriptSettings.MixerFollowMessage.format(name=message.Name)
+				SendTTSMessagesWithDelay(ttsMessage, ScriptSettings.MixerFollowDelay)
+
+		elif evntdata.Type == "subscription" and ScriptSettings.MixerOnSub:
+			for message in evntdata.Message:
+				ttsMessage = ''
+				if message.Months == 1:
+					ttsMessage = ScriptSettings.MixerSubMessage.format(name=message.Name, tier=tier, months=message.Months)
+				else:
+					ttsMessage = ScriptSettings.MixerResubMessage.format(name=message.Name, tier=tier, months=message.Months)
+
+				SendTTSMessagesWithDelay(ttsMessage, ScriptSettings.MixerSubDelay, ScriptSettings.MixerIncludeSubMessage, message.Message)
+
+		elif evntdata.Type == "host" and ScriptSettings.MixerOnHost:
+			s = ''
+			for message in evntdata.Message:
+				if int(message.Viewers) >= ScriptSettings.MixerHostMinimum:
+					if message.Viewers > 1:
+						s = 's'
+					else:
+						s = ''
+					ttsMessage = ScriptSettings.MixerHostMessage.format(name=message.Name, amount=str(message.Viewers), isPlural=s)
+					SendTTSMessagesWithDelay(ttsMessage, ScriptSettings.MixerHostDelay)
+
+	elif evntdata and evntdata.For == "streamlabs":
+		# This is a streamlabs donation event
+		if evntdata.Type == "donation" and ScriptSettings.StreamlabsOnDonation:
+			for message in evntdata.Message:
+				if float(message.Amount) >= ScriptSettings.StreamlabsDonationMinimum:
+					ttsMessage = ScriptSettings.StreamlabsDonationMessage.format(name=message.Name, amount=str(message.FormattedAmount))
+					SendTTSMessagesWithDelay(ttsMessage, ScriptSettings.StreamlabsDonationDelay, ScriptSettings.StreamlabsIncludeDonationMessage, message.Message)
+
+	elif evntdata and evntdata.For == "youtube_account":
+
+		# This is an Twitch follow event
+		if evntdata.Type == "follow" and ScriptSettings.YoutubeOnFollow:
+			# Events can come in bulk so it is in a list, itterate over it.
+			for message in evntdata.Message:
+				ttsMessage = ScriptSettings.YoutubeFollowMessage.format(name=message.Name)
+				SendTTSMessagesWithDelay(ttsMessage, ScriptSettings.YoutubeFollowDelay)
+
+		elif evntdata.Type == "subscription" and ScriptSettings.YoutubeOnSub:
+			for message in evntdata.Message:
+				ttsMessage = ''
+				if message.Months == 1:
+					ttsMessage = ScriptSettings.YoutubeSubMessage.format(name=message.Name, tier=tier, months=message.Months)
+				else:
+					ttsMessage = ScriptSettings.YoutubeResubMessage.format(name=message.Name, tier=tier, months=message.Months)
+
+				SendTTSMessagesWithDelay(ttsMessage, ScriptSettings.YoutubeSubDelay)
+
+		elif evntdata.Type == "superchat" and ScriptSettings.YoutubeOnSuperchat:
+			for message in evntdata.Message:
+				if float(message.Amount) >= ScriptSettings.YoutubeSuperchatMinimum:
+					ttsMessage = ScriptSettings.YoutubeSuperchatMessage.format(name=message.Name, amount=str(message.FormattedAmount))
+					SendTTSMessagesWithDelay(ttsMessage, ScriptSettings.YoutubeSuperchatDelay, ScriptSettings.YoutubeIncludeSuperchatMessage, message.Message)
+
+#---------------------------------------
+# Script Functions
+#---------------------------------------
+def updateUIConfig():
+	voices = []
+	for voice in spk.GetInstalledVoices():
+		info = voice.VoiceInfo
+		voices.append(info.Name)
+
+	UIConfigs = UIConfig(UIConfigFile)
+	UIConfigs.VoiceName['items'] = voices
+
+	if ScriptSettings.VoiceName not in voices:
+		ScriptSettings.VoiceName = ''
+		ScriptSettings.Save(SettingsFile)
+
+	UIConfigs.Save(UIConfigFile)
+
+def SendTTSMessage(message):
+	Parent.Log(ScriptName, message)
+	global spk
+	spk.Speak(message)
+
+def SendTTSMessagesWithDelay(message, delay, includeExtra = False, extraMessage = ''):
+	if delay > 0:
+		time.sleep(delay)
+
+	SendTTSMessage(message)
+	if includeExtra:
+		SendTTSMessage(extraMessage)
+
+#---------------------------------------
+# Chatbot Initialize Function
+#---------------------------------------
+def Init():
+
+	# Load settings from file and verify
+	global ScriptSettings
+	ScriptSettings = Settings(SettingsFile)
+
+	global spk
+	spk = SpeechSynthesizer()
+	spk.Rate = ScriptSettings.Rate
+	spk.Volume = ScriptSettings.Volume
+
+	updateUIConfig()
+
+	if ScriptSettings.VoiceName != '':
+		spk.SelectVoice(ScriptSettings.VoiceName)
+
+	# Init the Streamlabs Event Receiver
+	global EventReceiver
+	EventReceiver = StreamlabsEventClient()
+	EventReceiver.StreamlabsSocketConnected += EventReceiverConnected
+	EventReceiver.StreamlabsSocketDisconnected += EventReceiverDisconnected
+	EventReceiver.StreamlabsSocketEvent += EventReceiverEvent
+
+	# Auto Connect if key is given in settings
+	if ScriptSettings.SocketToken:
+		EventReceiver.Connect(ScriptSettings.SocketToken)
+
+	# End of Init
+	return
+
+#---------------------------------------
+# Chatbot Save Settings Function
+#---------------------------------------
+def ReloadSettings(jsondata):
+
+	# Reload newly saved settings and verify
+	ScriptSettings.Reload(jsondata)
+
+	if ScriptSettings.VoiceName != '':
+		global spk
+		spk.SelectVoice(ScriptSettings.VoiceName)
+
+	spk.Rate = ScriptSettings.Rate
+	spk.Volume = ScriptSettings.Volume
+
+	# End of ReloadSettings
+	return
+
+#---------------------------------------
+# Chatbot Execute Function
+#---------------------------------------
+def Execute(data):
+
+	if data.IsChatMessage():
+		if ScriptSettings.TTSAllChat:
+			message = ScriptSettings.TTSAllChatMessage.format(user=data.UserName, message=data.Message)
+			SendTTSMessage(message)
+
+		else:
+			command = data.GetParam(0)
+
+			if command == ScriptSettings.TTSCommand:
+				if HasPermission(data, ScriptSettings.TTSCommandPermission, ScriptSettings.TTSCommandPermissionInfo):
+					if not IsOnCooldown(data, ScriptSettings.TTSCommand, ScriptSettings.TTSCasterCD, ScriptSettings.TTSUseCD, ScriptSettings.TTSOnCooldown, ScriptSettings.TTSOnUserCooldown):
+						if HasCurrency(data, ScriptSettings.TTSCommandCost):
+							commandOffset = len(ScriptSettings.TTSCommand) + 1
+							message = ScriptSettings.TTSCommandMessage.format(user=data.UserName, message=data.Message[commandOffset:])
+							SendTTSMessage(message)
+
+							Parent.AddUserCooldown(ScriptName, ScriptSettings.TTSCommand, data.User, ScriptSettings.TTSUserCooldown)
+	    					Parent.AddCooldown(ScriptName, ScriptSettings.TTSCommand, ScriptSettings.TTSCooldown)
+
+	# End of execute
+	return
+
+#---------------------------------------
+# Chatbot Execute Helper Functions
+#---------------------------------------
+def HasPermission(data, permission, permissionInfo):
+    """Returns true if user has permission and false if user doesn't"""
+    if not Parent.HasPermission(data.User, permission, permissionInfo):
+        return False
+    return True
+
+def IsOnCooldown(data, command, casterCD, useCD, cooldownMessage, userCooldownMessage):
+    """Return true if command is on cooldown and send cooldown message if enabled"""
+    #introduce globals for cooldown management
+    cooldown = Parent.IsOnCooldown(ScriptName, command)
+    userCooldown = Parent.IsOnUserCooldown(ScriptName, command, data.User)
+    caster = (Parent.HasPermission(data.User, "Caster", "") and casterCD)
+
+    #check if command is on cooldown
+    if (cooldown or userCooldown) and caster is False:
+
+        #check if cooldown message is enabled
+        if useCD:
+
+            #set variables for cooldown
+            cooldownDuration = Parent.GetCooldownDuration(ScriptName, command)
+            userCDD = Parent.GetUserCooldownDuration(ScriptName, command, data.User)
+
+            #check for the longest CD!
+            if cooldownDuration > userCDD:
+
+                #set cd remaining
+                m_CooldownRemaining = cooldownDuration
+
+                #send cooldown message
+                message = cooldownMessage.format(user=data.UserName, cooldown=m_CooldownRemaining)
+                Parent.SendStreamMessage(message)
+
+            else: #set cd remaining
+                m_CooldownRemaining = userCDD
+
+                #send usercooldown message
+                message = userCooldownMessage.format(user=data.UserName, cooldown=m_CooldownRemaining)
+                Parent.SendStreamMessage(message)
+        return True
+    return False
+
+def HasCurrency(data, cost):
+    if (cost == 0) or (Parent.RemovePoints(data.User, data.UserName, cost)):
+        return True
+    return False
+
+#---------------------------------------
+# Chatbot Tick Function
+#---------------------------------------
+def Tick():
+
+	# End of Tick
+	return
+
+#---------------------------------------
+# Chatbot Parameter Parser
+#---------------------------------------
+def Parse(parseString, user, target, message):
+
+	# Return unaltered parseString
+	return parseString
+
+#---------------------------------------
+# Chatbot Button Function
+#---------------------------------------
+def OpenReadMe():
+    """Open the README.txt in the scripts folder"""
+    os.startfile(os.path.join(os.path.dirname(__file__), "README.txt"))
+
+def OpenSocketToken():
+    """Open Streamlabs API Settings"""
+    OpenLink("https://streamlabs.com/dashboard#/settings/api-settings")
+
+def OpenGithubRepository():
+	"""Open the GitHub Repository for this script"""
+	OpenLink("https://github.com/kruiser8/TTS-Alerts-And-Chat")
+
+def OpenTwitter():
+	"""Open the Twitter of the author"""
+	OpenLink("https://twitter.com/kruiser8")
+
+def OpenLink(link):
+    """Open links through buttons in UI"""
+    os.system("explorer " + link)
